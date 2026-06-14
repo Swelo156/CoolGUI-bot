a45bb776e35bac98a9fa7775423f9147a5fa4e67
Swelo156 <gilsontyson15@outlook.com>
2026-06-14 16:48:54 +1000

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "The one and only CoolGuiBOT",
+        name: "The one and only Apex Bot",
         // Activity type number (0 = Playing).
         type: 0, 
       },
